2942187f9c538e16bcbf946730c0dd7c325f18a8
clay <clayjwin@gmail.com>
2024-11-05 08:40:39 -0800

    Update NanoLabGUI_v01_4.py

--- complete_project_WINDOWS/NanoLabGUI_v01_4.py
+++ complete_project_WINDOWS/NanoLabGUI_v01_4.py
@@ -162,12 +162,18 @@ def load_settings_frame():
 	command=lambda:load_settings_frame()
 	print("settings loaded")
 
-
-
-#HI
-
-
+ser = serial.Serial('COM3')
+# open serial port
  
+print(ser.name)
+ # check which port was really used
+ 
+ser.write(b'hello')
+ # write a string
+ 
+ser.close()
+ # close port	
+
 # load the first frame and button bar
 load_menu()
 load_settings_frame()
