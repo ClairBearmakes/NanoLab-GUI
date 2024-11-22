7bdc739610d6ec7693fc4f7e8ff057d2fced9a46
Ash <91293003+4ecretman@users.noreply.github.com>
2024-11-22 10:16:51 -0700

    Update sketch_nov20a.ino

--- complete_project_WINDOWS/sketch_nov20a/sketch_nov20a.ino
+++ complete_project_WINDOWS/sketch_nov20a/sketch_nov20a.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 int pin = 6;
